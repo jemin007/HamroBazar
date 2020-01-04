da8765762db7aeb461713e29f621ede7e50bdf52
jemin <jeminshrestha9@gmail.com>
2020-01-04 19:41:01 +0545

    Splash complete and aggrement display complete

---
+++ app/src/main/java/com/example/hamrobazar/FirstAgreement.java
@@ -0,0 +1,14 @@
+package com.example.hamrobazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstAgreement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_agreement);
+    }
+}
